5e6174054f7ccacd705056e30819518e7ba96155
jfaldanam <jfaldanam@gmail.com>
2018-05-26 13:30:52 +0200

    Now receive MySQL user and pswd as arguments

--- ScamChecker/src/scamChecker/Driver.java
+++ ScamChecker/src/scamChecker/Driver.java
@@ -4,8 +4,13 @@ public class Driver {
 	
 	public static void main(String[] args) {
 		
-		String userMysql = "root"; //INTRODUCE USER OF LOCAL MYSQL
-		String passwordMysql = ""; //INTRODUCE PASSWORD OF LOCAL MYSQL
+		if (args.length < 2) {
+			System.err.println("Pass as parameters MySQL user and password");
+			System.exit(1);
+		}
+
+		String userMysql = args[0]; //INTRODUCE USER OF LOCAL MYSQL
+		String passwordMysql = args[1]; //INTRODUCE PASSWORD OF LOCAL MYSQL
 		
 		BDController bd = new BDController(userMysql, passwordMysql);
 		View v = new View();
